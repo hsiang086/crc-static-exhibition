1c08b46b0992ffed852fcee54cbf3ddd81c2df6e
hsiang086 <115937789+hsiang086@users.noreply.github.com>
2023-03-10 23:44:32 +0800

    Update runner_video.py

--- UltimatePygameIntro-main/runner_video.py
+++ UltimatePygameIntro-main/runner_video.py
@@ -174,6 +174,7 @@ player_gravity = 0
 player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
 player_stand = pygame.transform.rotozoom(player_stand,0,2)
 player_stand_rect = player_stand.get_rect(center = (400,200))
+pygame.display.set_icon(player_stand)
 
 game_name = test_font.render('Pixel Runner',False,(111,196,169))
 game_name_rect = game_name.get_rect(center = (400,80))
