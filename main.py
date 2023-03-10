652cc7effe06233c4232de60a69a8382ce3fa1d6
hsiang086 <115937789+hsiang086@users.noreply.github.com>
2023-03-10 20:06:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import os
 import pygame as pg
 
-surface = pg.display.get_surface()
+black = (0, 0, 0)
 white = (255, 255, 255)
-RES = W, H = 0,0
+RES = W, H = 600, 600
 FPS = 60
+
+center_x = W//2
+center_y = H//2
+speed = 5
+player = [center_x,center_y]
 
 pg.init()
 
 clock = pg.time.Clock()
-screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+screen = pg.display.set_mode(RES)
+font = pg.font.SysFont("arial", 20, bold=True)
+
+def text_display(char, x, y):
+    text = font.render(str(char), True, black)
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
 
 running = True
 while running:
     clock.tick(FPS)
     pg.display.set_caption("main")
     screen.fill(white)
+
+    text_display("Hello world",player[0],player[1])
+
+
     pg.display.update()
     for event in pg.event.get():
         if event.type == pg.QUIT:
