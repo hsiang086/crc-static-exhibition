b3661da08ba9e48e99f676507bd82bd2e5058445
AndySwitch <190030@gs.hs.ntnu.edu.tw>
2023-03-30 21:37:30 +0800

    Add object

--- main/second_scene.py
+++ main/second_scene.py
@@ -1,13 +1,18 @@
 import pygame
+import random
 
 def init():
     global FPS
     global RES, WIDTH, HIGHT
     global clock
     global screen
+    global POS
 
     FPS = 60
     RES = WIDTH, HIGHT = (1600, 900)
+
+    POS=[(HIGHT/2)-200,(HIGHT/2),(HIGHT/2)+200]
+
     clock = pygame.time.Clock()
     
     itv = 0
@@ -28,7 +33,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 10
         self.rect.centery = HIGHT/2
-        self.pos = 0
+        self.pos = 1
         self.itv = 0
          
     def update(self):
@@ -36,37 +41,66 @@ class Player(pygame.sprite.Sprite):
         key_pressed = pygame.key.get_pressed()
         if self.itv >= 8:
             if key_pressed[pygame.K_UP]:
-                if self.pos == -1:
-                    self.pos = -1
+                if self.pos == 0:
+                    self.pos = 0
                 else:
-                    self.pos -= 1
-                while self.rect.centery >= (HIGHT/2) + self.pos*200:
-                    self.rect.centery -= 2 
+                    self.pos -= 1 
             if key_pressed[pygame.K_DOWN]:
-                if self.pos == 1:
-                    self.pos = 1
+                if self.pos == 2:
+                    self.pos = 2
                 else:
-                    self.pos += 1
-                self.rect.centery = (HIGHT/2) + self.pos*200      
-            self.itv = 0     
+                    self.pos += 1     
+            self.itv = 0
+        self.rect.centery = POS[self.pos]         
+
+class Object(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill((0,153,153))
+        self.rect = self.image.get_rect()
+        self.rect.left = WIDTH
+        self.pos = random.randrange(0,3)
+        self.rect.centery = POS[self.pos]
+        self.v = -5
+        self.itv = 0
+    def update(self):
+        if self.rect.right >= 0:
+            self.rect.x += self.v
+        else:    
+            self.kill()
+
+
 
         
         
 all_sprites = pygame.sprite.Group()
+objects = pygame.sprite.Group() 
 player = Player()
 all_sprites.add(player) 
 
 
+time = 0
 running = True
 while running:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-           
-    all_sprites.update()   
+    if time >= 90:
+        Obj = Object()
+        all_sprites.add(Obj)
+        objects.add(Obj)  
+        time = 0
+
+    all_sprites.update()
+
+    hits = pygame.sprite.spritecollide(player, objects, 0)
+    if hits:
+        running = False
     screen.fill((255,255,255))
     all_sprites.draw(screen)
-    pygame.display.update()        
+    pygame.display.update()
+    time += 1        
 
 pygame.quit()
