02497d284168e7fd6b587bf228ca417a5b7d0026
Wanland086 <122431192+Wanland086@users.noreply.github.com>
2023-03-30 22:37:04 +0800

    Update second_scene.py

--- main/second_scene.py
+++ main/second_scene.py
@@ -1,4 +1,6 @@
 import pygame
+from time import sleep
+import random
 
 def init():
     global FPS
@@ -10,7 +12,6 @@ def init():
     RES = WIDTH, HIGHT = (1200, 700)
     clock = pygame.time.Clock()
     
-    itv = 0
     pygame.init()
     screen = pygame.display.set_mode(RES)
     
@@ -26,35 +27,57 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((100,100))
         self.image.fill((0,255,0))
         self.rect = self.image.get_rect()
-        self.rect.x = 10
-        self.rect.centery = HIGHT/2
-        self.pos = 0
-        self.itv = 0
+        self.rect.centerx = 100
+        self.rect.centery = HIGHT / 2
+        self.position = 0
+        # self.pos = 0
+        # self.itv = 0
          
     def update(self):
-        self.itv += 1
-        key_pressed = pygame.key.get_pressed()
-        if self.itv >= 8:
-            if key_pressed[pygame.K_UP]:
-                if self.pos == -1:
-                    self.pos = -1
-                else:
-                    self.pos -= 1
-                while self.rect.centery >= (HIGHT/2) + self.pos*200:
-                    self.rect.centery -= 2 
-            if key_pressed[pygame.K_DOWN]:
-                if self.pos == 1:
-                    self.pos = 1
-                else:
-                    self.pos += 1
-                self.rect.centery = (HIGHT/2) + self.pos*200      
-            self.itv = 0     
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    if self.rect.centery > HIGHT / 2 - 200 :
+                        self.position -= 1
+                if event.key == pygame.K_s:
+                    if self.rect.centery < HIGHT / 2 + 200 :
+                        self.position += 1
+        self.rect.centery += self.position * 200
+        self.position = 0
+        # self.itv += 1
+        # key_pressed = pygame.key.get_pressed()
+        # if self.itv >= 8:
+        #     if key_pressed[pygame.K_UP] or key_pressed[pygame.K_w]:
+        #         if self.pos == -1:
+        #             self.pos = -1
+        #         else:
+        #             self.pos -= 1
+        #         while self.rect.centery > (HIGHT / 2) + self.pos * 200:
+        #             self.rect.centery -= 2 
+        #     if key_pressed[pygame.K_DOWN] or key_pressed[pygame.K_s]:
+        #         if self.pos == 1:
+        #             self.pos = 1
+        #         else:
+        #             self.pos += 1
+        #         self.rect.centery = (HIGHT / 2) + self.pos * 200      
+        #     self.itv = 0     
 
-        
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((100,100))
+        self.image.fill('red')
+        self.rect = self.image.get_rect()
+        self.rect.centery = ( HIGHT / 2 ) + ( 200 * ( random.randint(-1, 1) ) )
+        self.rect.centerx = 0#WIDTH + 10
+
+    
+
         
 all_sprites = pygame.sprite.Group()
 player = Player()
-all_sprites.add(player) 
+obstacle = Obstacle()
+all_sprites.add(player, obstacle) 
 
 
 running = True
